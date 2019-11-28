d429c9138d6bec9e9a90eeee2ded28ef6f6177ec
Kenneth Lausdahl <kgl@mjolner.dk>
2019-11-28 23:53:12 +0200

    jenkins

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
 rm -rf target
 mkdir target
-docker run -it \
+docker run  \
     -v "$(pwd)":/application \
     docker.sweng.au.dk/haskell865nodeelm:latest \
     /bin/bash -c 'cd application && ./make.sh 'none''
