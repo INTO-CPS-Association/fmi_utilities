59afee85aaad74015e42c1df902982e0c0c15d02
Casper Thule <au443759@d39645.st.client.au.dk>
2020-02-03 14:06:57 +0100

    Downgraded, added markdown, fixed #9

--- frontend/src/Page/FMIChecker.elm
+++ frontend/src/Page/FMIChecker.elm
@@ -35,13 +35,27 @@ init basePath =
     ( Model Nothing Nothing basePath FMI2, Cmd.none )
 
 
+textBody : String
+textBody =
+    """The FMI specification is semi-formal. This can lead to different interpretations, which can in turn lead to different FMU implementations. The FMI Checker is an attempt to formalise the static constraints of the FMI standard - the model description file - and provide a tool to automatically check an FMU.
+
+The formalisation is defined as a VDM-SL specification, which is used to check the uploaded model description file. The VDM model and source code of the FMI Checker is available at: [https://github.com/INTO-CPS-Association/FMI-VDM-Model](https://github.com/INTO-CPS-Association/FMI-VDM-Model).
+
+If you disagree with a checking result then we are very interested to hear about it! This will allow us to pin down a more accurate interpretation of the standard, and (possibly) correct issues in the tools. Please report issues here: [https://github.com/INTO-CPS-Association/FMI-VDM-Model/issues](https://github.com/INTO-CPS-Association/FMI-VDM-Model/issues).
+
+This approach is also presented in: [Towards a Static Check of FMUs in VDM-SL.](https://eprints.ncl.ac.uk/260975) / Battle, Nick; Thule, Casper; Gomes, Claudio ; Macedo, Hugo Daniel; Larsen, Peter Gorm.
+Proceedings of the 17th Overture Workshop. red. / Carl Gamble; Luis Diogo Couto. Newcastle University, 2019. s. 17-32 (Technical Report Series; Nr. CS-TR- 1530 - 2019)."""
+
+
 view : Model -> Html Msg
 view model =
     div
         [ class "card" ]
         [ div [ class "card-header" ] [ text "FMI Checker" ]
         , div [ class "card-body" ]
-            [ div
+            [ Utilities.markdownToString textBody
+            , Html.hr [ style "color" "black", style "margin-left" "0", style "margin-right" "0" ] []
+            , div
                 [ class "form-group" ]
                 [ label [ for "fileSelector" ]
                     [ text "Model Description or FMU:"
--- frontend/src/Utilities.elm
+++ frontend/src/Utilities.elm
@@ -1,12 +1,16 @@
-module Utilities exposing (filesDecoder)
+module Utilities exposing (filesDecoder, markdownToString)
 
 import File exposing (..)
+import Html exposing (..)
 import Json.Decode as D
+import Markdown.Parser
 
 
 
 -- Even if multiple is False the result is always a list, just with 1 entry.
 -- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#files
+-- "target" originate from: https://developer.mozilla.org/en-US/docs/Web/API/Event/target
+-- "files" originate from: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#Additional_attributes
 
 
 filesDecoder : D.Decoder (List File)
@@ -15,5 +19,25 @@ filesDecoder =
 
 
 
--- "target" originate from: https://developer.mozilla.org/en-US/docs/Web/API/Event/target
--- "files" originate from: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#Additional_attributes
+{- https://github.com/dillonkearns/elm-markdown/tree/2.0.0 -}
+
+
+markdownToString : String -> Html msg
+markdownToString mdText =
+    case
+        mdText
+            |> Markdown.Parser.parse
+            |> Result.mapError deadEndsToString
+            |> Result.andThen (\ast -> Markdown.Parser.render Markdown.Parser.defaultHtmlRenderer ast)
+    of
+        Ok rendered ->
+            div [] rendered
+
+        Err errors ->
+            text errors
+
+
+deadEndsToString deadEnds =
+    deadEnds
+        |> List.map Markdown.Parser.deadEndToString
+        |> String.join "\n"
