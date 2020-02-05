9660335a8bfbef79a3eeb69d45f33096c0c77d81
Casper Thule <au443759@d39645.st.client.au.dk>
2020-02-05 12:48:43 +0100

    updated home text

--- frontend/src/Page/Home.elm
+++ frontend/src/Page/Home.elm
@@ -23,10 +23,14 @@ textBody =
     """
 This website is the frontend for various FMI-related activities emerging from the INTO-CPS Association.
 
+
 Information on the activities the are representing on this website is on the page of the activity in question.
 Currently the activities are the following: 
 - **FMI Checker** checks the static semantics of a model description file of an FMU.
 - **FMU Analyzer** exercises an FMU by loading it an invoking various functions. It takes the model description file of a given FMU into consideration when setting and getting variables.
+
+Several activities are related to supporting the community in rooting
+out possible ambiguities and improving the conformance to the FMI standard.
 
 For website issues, please report these at: [https://github.com/into-cps-association/fmi_utilities/issues](https://github.com/into-cps-association/fmi_utilities/issues.)        
 
