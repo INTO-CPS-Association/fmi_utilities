479377fff386cd4879698b5146e36e021f98e73b
Casper Thule <casper.thule@eng.au.dk>
2020-12-07 10:26:15 +0100

    Updated vdmcheck

--- vdmcheck-0.0.2/VDMCheck2.sh
+++ vdmcheck-0.0.2/VDMCheck2.sh
@@ -31,30 +31,62 @@
 # Process an FMI V2 FMU or XML file, and validate the XML structure using the VDM-SL model.
 #
 
-if [ "$1" = "-v" -a $# -gt 2 ]
-then
-	SAVE=$2
-	shift 2
-fi
+USAGE="Usage: VDMCheck2.sh [-v <VDM outfile>] [-s <XSD>] -x <XML> | <file>.fmu | <file>.xml"
 
-if [ $# -ne 1 ]
+while getopts ":v:x:s:" OPT
+do
+    case "$OPT" in
+        v)
+            SAVE=${OPTARG}
+            ;;
+        x)
+            INXML=${OPTARG}
+            ;;
+        s)
+        	INXSD=${OPTARG}
+        	;;
+        *)
+			echo "$USAGE"
+			exit 1
+            ;;
+    esac
+done
+
+shift "$((OPTIND-1))"
+
+if [ $# = 1 ]
 then
-	echo "Usage: $0 [-v <VDM outfile>] <FMU or modelDescription.xml file>"
-	exit 1
-else
 	FILE=$1
 fi
 
-if [ ! -e "$FILE" ]
+if [ "$INXML" -a "$FILE" ] || [ -z "$INXML" -a -z "$FILE" ]
+then
+	echo "$USAGE"
+	exit 1
+fi
+
+if [ ! -z "$FILE" -a ! -e "$FILE" ]
 then
 	echo "File not found: $FILE"
 	exit 1
 fi
 
+if [ "$INXML" ]
+then
+	FILE=/tmp/xml$$.xml
+	TMPX=$FILE
+	echo "$INXML" >$FILE
+fi
+
+if [ "$INXSD" ]
+then
+	INXSD=$(readlink -f "$INXSD")
+fi 
+
 XML=/tmp/modelDescription$$.xml
 VDM=/tmp/vdm$$.vdmsl
 
-trap "rm -f $XML $VDM" EXIT
+trap "rm -f $XML $VDM $TMPX" EXIT
 
 case $(file -b --mime-type $FILE) in
 	application/zip)
@@ -83,10 +115,15 @@ esac
 
 # Subshell cd, so we can set the classpath
 (
-	dir=$(dirname "$0")
+	path=$(which "$0")
+	dir=$(dirname "$path")
 	cd "$dir"
-	
 	VAR=model$$
+	
+	if [ ! "$INXSD" ]
+	then
+		INXSD="schema/fmi2ModelDescription.xsd"
+	fi
 	
 	if ! type java 2>/dev/null 1>&2
 	then
@@ -94,20 +131,25 @@ esac
 		exit 2
 	fi
 	
-	if ! java -jar fmi2vdm-0.0.2.jar "$XML" "$VAR" >$VDM
+	if ! java -cp fmi2vdm.jar fmi2vdm.FMI2SaxParser "$XML" "$VAR" "$INXSD" >$VDM
 	then
 		echo "Problem converting modelDescription.xml to VDM-SL?"
 		exit 2
 	fi
 	
-	java -Xmx1g -cp vdmj-4.3.0.jar:annotations-1.0.0.jar:annotations2-1.0.0.jar \
-		com.fujitsu.vdmj.VDMJ \
+	java -Xmx1g -cp vdmj.jar:annotations.jar com.fujitsu.vdmj.VDMJ \
 		-vdmsl -q -annotations -e "isValidFMIModelDescription($VAR)" \
-		model $VDM | sed -e "s/true/No errors found./; s/false/Errors found./"
+		model $VDM |
+		awk '/^true$/{ print "No errors found."; exit 0 };/^false$/{ print "Errors found."; exit 1 };{ print }'
 )
+
+EXIT=$?		# From subshell above
 
 if [ "$SAVE" ]
 then
+	if [ $FILE="" ]; then FILE="XML"; fi
 	sed -e "s+generated from $XML+generated from $FILE+" $VDM > "$SAVE"
 	echo "VDM source written to $SAVE"
 fi
+
+exit $EXIT
--- vdmcheck-0.0.3/VDMCheck3.sh
+++ vdmcheck-0.0.3/VDMCheck3.sh
@@ -28,33 +28,68 @@
 # See the full INTO-CPS Association Public License conditions for more details.
 
 #
-# Process an FMI V2 FMU or XML file, and validate the XML structure using the VDM-SL model.
+# Process an FMI V3 FMU or XML file, and validate the XML structure using the VDM-SL model.
 #
 
-if [ "$1" = "-v" -a $# -gt 2 ]
-then
-	SAVE=$2
-	shift 2
-fi
+USAGE="Usage: VDMCheck3.sh [-v <VDM outfile>] [-s <XSD>] -x <XML> | <file>.fmu | <file>.xml"
 
-if [ $# -ne 1 ]
+while getopts ":v:x:s:" OPT
+do
+    case "$OPT" in
+        v)
+            SAVE=${OPTARG}
+            ;;
+        x)
+            INXML=${OPTARG}
+            ;;
+        s)
+        	INXSD=${OPTARG}
+        	;;
+        *)
+			echo "$USAGE"
+			exit 1
+            ;;
+    esac
+done
+
+shift "$((OPTIND-1))"
+
+if [ $# = 1 ]
 then
-	echo "Usage: $0 [-v <VDM outfile>] <FMU or modelDescription.xml file>"
-	exit 1
-else
 	FILE=$1
 fi
 
-if [ ! -e "$FILE" ]
+if [ "$INXML" -a "$FILE" ] || [ -z "$INXML" -a -z "$FILE" ]
+then
+	echo "$USAGE"
+	exit 1
+fi
+
+if [ ! -z "$FILE" -a ! -e "$FILE" ]
 then
 	echo "File not found: $FILE"
 	exit 1
 fi
 
-XML=/tmp/modelDescription$$.xml
+if [ "$INXML" ]
+then
+	FILE=/tmp/input$$.xml
+	echo "$INXML" >$FILE
+	INXML=$FILE
+fi
+
+if [ "$INXSD" ]
+then
+	INXSD=$(readlink -f "$INXSD")
+fi 
+
+XML_MD=/tmp/modelDescription$$.xml
+XML_BD=/tmp/buildDescription$$.xml
+XML_TI=/tmp/terminalsAndIcons$$.xml
+XML_XM=/tmp/xml$$.xml
 VDM=/tmp/vdm$$.vdmsl
 
-trap "rm -f $XML $VDM" EXIT
+trap "rm -f $XML_MD $XML_BD $XML_TI $XML_XM $INXML $VDM" EXIT
 
 case $(file -b --mime-type $FILE) in
 	application/zip)
@@ -64,15 +99,36 @@ case $(file -b --mime-type $FILE) in
 			exit 2
 		fi
 		
-		if ! unzip -p "$FILE" modelDescription.xml >$XML
+		if ! unzip -p "$FILE" modelDescription.xml >$XML_MD
 		then
-			echo "Problem with unzip of $FILE?"
+			echo "Problem with unzip of modelDescription.xml?"
 			exit 2
 		fi
+		
+		TMPX=/tmp/temp$$.xml
+		
+		if unzip -p "$FILE" source/buildDescription.xml >$TMPX 2>/dev/null
+		then
+			cp $TMPX $XML_BD
+		else
+			rm -f $XML_BD
+		fi
+		
+		if unzip -p "$FILE" icon/terminalsAndIcons.xml >$TMPX 2>/dev/null
+		then
+			cp $TMPX $XML_TI
+		else
+			rm -f $XML_TI
+		fi
+		
+		rm -f $TMPX $XML_XM
 	;;
 		
 	application/xml|text/xml)
-		cp $FILE $XML
+		cp $FILE $XML_XM
+		rm -f $XML_MD
+		rm -f $XML_BD
+		rm -f $XML_TI
 	;;
 		
 	*)
@@ -81,33 +137,82 @@ case $(file -b --mime-type $FILE) in
 	;;
 esac
 
-# Subshell cd, so we can set the classpath
-(
-	dir=$(dirname "$0")
-	cd "$dir"
-	
-	VAR=model$$
-	
-	if ! type java 2>/dev/null 1>&2
+SCRIPT=$0
+
+function check()	# $1 = the XML temp file to check, $2 = name of the file 
+{
+	if [ ! -e "$1" ]
 	then
-		echo "java is not installed?"
-		exit 2
+		return 0
 	fi
 	
-	if ! java -jar fmi3vdm-0.0.3.jar "$XML" "$VAR" >$VDM
+	echo "Checking $2"
+	
+	# Subshell cd, so we can set the classpath
+	(
+		path=$(which "$SCRIPT")
+		dir=$(dirname "$path")
+		cd "$dir"
+		VAR=model$$
+		
+		if [ ! "$INXSD" ]
+		then
+			INXSD="schema/fmi3.xsd"
+		fi
+	
+		if ! type java 2>/dev/null 1>&2
+		then
+			echo "java is not installed?"
+			exit 2
+		fi
+		
+		java -cp fmi2vdm.jar fmi2vdm.FMI3SaxParser "$1" "$VAR" "$INXSD" >$VDM
+		
+		if [ $? -ne 0 ]
+		then
+			echo "Problem converting $1 to VDM-SL?"
+			exit 2
+		fi
+		
+		java -Xmx1g -cp vdmj.jar:annotations.jar com.fujitsu.vdmj.VDMJ \
+			-vdmsl -q -annotations -e "isValidFMIConfiguration($VAR)" \
+			model $VDM |
+			awk '/^true$/{ print "No errors found."; exit 0 };/^false$/{ print "Errors found."; exit 1 };{ print }'
+	)
+	
+	RET=$?		# From subshell above
+	
+	if [ "$SAVE" ]
 	then
-		echo "Problem converting modelDescription.xml to VDM-SL?"
-		exit 2
+		if [ "$FILE" = "" ]; then FILE="XML"; fi
+		sed -e "s+generated from $1+generated from $2+" $VDM >> "$SAVE"
+		echo "VDM source written to $SAVE"
 	fi
 	
-	java -Xmx1g -cp vdmj-4.3.0-P.jar:annotations-1.0.0.jar:annotations2-1.0.0.jar \
-		com.fujitsu.vdmj.VDMJ \
-		-vdmsl -q -annotations -e "isValidFMIModelDescription($VAR)" \
-		model $VDM | sed -e "s/^true$/No errors found./; s/^false$/Errors found./"
-)
+	return $RET
+}
 
 if [ "$SAVE" ]
 then
-	sed -e "s+generated from $XML+generated from $FILE+" $VDM > "$SAVE"
-	echo "VDM source written to $SAVE"
+	rm -f "$SAVE"
 fi
+
+EXIT=0
+
+if ! check "$XML_XM" XML
+then EXIT=1
+fi
+
+if ! check "$XML_MD" modelDescription.xml
+then EXIT=1
+fi
+
+if ! check "$XML_BD" source/buildDescription.xml
+then EXIT=1
+fi
+
+if ! check "$XML_TI" icon/terminalsAndIcons.xml
+then EXIT=1
+fi
+
+exit $EXIT
